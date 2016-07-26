f3aab9e5070b2f6f9e8f1d79038e878c692dc1ab
iansoft <anziloveanzi@hotmail.com>
2016-07-27 00:03:09 +0800

    start ec6 with babel

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,19 @@
 //包装函数
 module.exports = function(grunt) {
+    // npm install --save-dev load-grunt-tasks 
+    require('load-grunt-tasks')(grunt); 
+    
     //任务配置
     grunt.initConfig({
         //获取 package.json 的信息
         pkg: grunt.file.readJSON('package.json'),
+        //检查js文件
+        jshint:{
+            build:["Gruntfile.js","src/*.js"],
+            options:{
+                jshintrc:'.jshintrc'
+            }
+        },
         //压缩js文件
         uglify:{
             options: {
@@ -14,19 +24,24 @@ module.exports = function(grunt) {
                 dest: 'build/<%= pkg.name %>.min.js'
             }
         },
-        //检查js文件
-        jshint:{
-            build:["Gruntfile.js","src/*.js"],
-            options:{
-                jshintrc:'.jshintrc'
-            }
-        },
         //监控
         watch:{
             build:{
                 files:["Gruntfile.js","src/*.js"],
                 tasks:['jshint','uglify'],
                 options:{spawn:false}
+            }
+        },
+        //babel for complie the EC6
+        babel: {
+            options: {
+                sourceMap: true,
+                presets: ['es2015']
+            },
+            dist: {
+                files: {
+                    'dist/app.js': 'src/app.js'
+                }
             }
         }
     });
@@ -37,5 +52,6 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks("grunt-contrib-watch");
 
     //创建自定义任务
-    grunt.registerTask('default', ['jshint','uglify','watch']);
+    //grunt.registerTask('default', ['jshint','uglify','watch']);
+    grunt.registerTask('ec6', ['babel']);
 };
